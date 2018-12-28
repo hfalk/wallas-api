c422c589819c2994a98d6edf762b739bc4ce6dcf
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-28 21:00:38 +0100

    Add twillio inbound request validation

--- src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
+++ src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
@@ -1,6 +1,5 @@
 package no.falcon.wallasapi.resource
 
-import mu.KotlinLogging
 import no.falcon.wallasapi.domain.TwillioInboundSms
 import no.falcon.wallasapi.properties.TwillioProperties
 import no.falcon.wallasapi.service.StatusService
@@ -8,11 +7,10 @@ import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
 import com.twilio.security.RequestValidator
-
+import org.springframework.http.ResponseEntity
 
 
 @RestController
@@ -22,16 +20,18 @@ class TwillioResource(
     private val request: HttpServletRequest,
     private val twillioProperties: TwillioProperties
 ) {
-    private val logger = KotlinLogging.logger {}
-
     @PostMapping(value = ["inbound"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    fun inbound(twillioInboundSms: TwillioInboundSms) {
-        validateTwillioRequest()
+    fun inbound(twillioInboundSms: TwillioInboundSms): ResponseEntity<Unit> {
+        if (!twillioRequestIsValid()) {
+            return ResponseEntity.status(HttpStatus.FORBIDDEN).build()
+        }
+
         statusService.insertStatus(twillioInboundSms.Body, twillioInboundSms.MessageSid)
+
+        return ResponseEntity.noContent().build()
     }
 
-    fun validateTwillioRequest() {
+    fun twillioRequestIsValid(): Boolean {
         val authToken = twillioProperties.authToken
 
         val validator = RequestValidator(authToken)
@@ -40,12 +40,6 @@ class TwillioResource(
         val params = request.parameterMap.map { prop -> prop.key to prop.value[0] }.toMap()
         val twilioSignature = request.getHeader("X-Twilio-Signature")
 
-        logger.info { url }
-        logger.info { params }
-        logger.info { twilioSignature }
-
-        val isValid = validator.validate(url, params, twilioSignature)
-
-        logger.info { isValid }
+        return validator.validate(url, params, twilioSignature)
     }
 }
