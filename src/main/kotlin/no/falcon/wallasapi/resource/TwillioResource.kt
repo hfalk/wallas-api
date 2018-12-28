6c602dbf81cf517e9d07fda5fc3e8a064dab70dc
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-28 15:47:39 +0100

    Return 204 for twillio/inbound

--- src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
+++ src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
@@ -3,6 +3,7 @@ package no.falcon.wallasapi.resource
 import mu.KotlinLogging
 import no.falcon.wallasapi.domain.TwillioInboundSms
 import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
@@ -13,7 +14,9 @@ class TwillioResource {
     private val logger = KotlinLogging.logger {}
 
     @PostMapping(value = ["inbound"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
-    fun inbound(twillioInboundSms: TwillioInboundSms) {
+    fun inbound(twillioInboundSms: TwillioInboundSms): ResponseEntity<Unit> {
         logger.info { twillioInboundSms }
+
+        return ResponseEntity.noContent().build()
     }
 }
