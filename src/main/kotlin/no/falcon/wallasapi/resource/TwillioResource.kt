8ba1bf5f5bc8c8f33aaef3a58844d5b679aecb61
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-28 20:51:21 +0100

    Impl twillio request validator

--- src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
+++ src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
@@ -2,6 +2,7 @@ package no.falcon.wallasapi.resource
 
 import mu.KotlinLogging
 import no.falcon.wallasapi.domain.TwillioInboundSms
+import no.falcon.wallasapi.properties.TwillioProperties
 import no.falcon.wallasapi.service.StatusService
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -10,12 +11,16 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 import javax.servlet.http.HttpServletRequest
+import com.twilio.security.RequestValidator
+
+
 
 @RestController
 @RequestMapping("twillio")
 class TwillioResource(
     private val statusService: StatusService,
-    private val request: HttpServletRequest
+    private val request: HttpServletRequest,
+    private val twillioProperties: TwillioProperties
 ) {
     private val logger = KotlinLogging.logger {}
 
@@ -27,12 +32,16 @@ class TwillioResource(
     }
 
     fun validateTwillioRequest() {
-        logger.info { "***********" }
-        logger.info { request.contextPath }
-        logger.info { request.queryString }
-        logger.info { request.requestURI }
-        logger.info { request.pathInfo }
-        logger.info { request.servletPath }
-        logger.info { "***********" }
+        val authToken = twillioProperties.authToken
+
+        val validator = RequestValidator(authToken)
+
+        val url = request.requestURL.toString()
+        val params = request.parameterMap.map { prop -> prop.key to prop.value[0] }.toMap()
+        val twilioSignature = request.getHeader("X-Twilio-Signature")
+
+        val isValid = validator.validate(url, params, twilioSignature)
+
+        logger.info { isValid }
     }
 }
