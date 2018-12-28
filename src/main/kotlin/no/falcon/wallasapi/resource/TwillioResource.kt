9897f2e327b35effa593480f5b82083024459f1e
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-28 15:58:59 +0100

    refactor

--- src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
+++ src/main/kotlin/no/falcon/wallasapi/resource/TwillioResource.kt
@@ -2,10 +2,11 @@ package no.falcon.wallasapi.resource
 
 import mu.KotlinLogging
 import no.falcon.wallasapi.domain.TwillioInboundSms
+import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.ResponseStatus
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -14,9 +15,8 @@ class TwillioResource {
     private val logger = KotlinLogging.logger {}
 
     @PostMapping(value = ["inbound"], consumes = [MediaType.APPLICATION_FORM_URLENCODED_VALUE])
-    fun inbound(twillioInboundSms: TwillioInboundSms): ResponseEntity<Unit> {
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    fun inbound(twillioInboundSms: TwillioInboundSms) {
         logger.info { twillioInboundSms }
-
-        return ResponseEntity.noContent().build()
     }
 }
