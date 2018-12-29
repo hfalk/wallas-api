cc5b0ba27ccf1842e75002cc2040dfa57e3a09d9
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2018-12-29 17:32:14 +0100

    Fix wallas response parse error

--- src/main/kotlin/no/falcon/wallasapi/domain/StatusContent.kt
+++ src/main/kotlin/no/falcon/wallasapi/domain/StatusContent.kt
@@ -2,8 +2,8 @@ package no.falcon.wallasapi.domain
 
 data class StatusContent(
     val rawValue: String,
-    val heaterStatus: String,
-    val setTemp: Double,
-    val readTemp: Double,
-    val volt: Double
+    val heaterStatus: String?,
+    val setTemp: Double?,
+    val readTemp: Double?,
+    val volt: Double?
 )
--- src/main/kotlin/no/falcon/wallasapi/util/WallasUtil.kt
+++ src/main/kotlin/no/falcon/wallasapi/util/WallasUtil.kt
@@ -1,26 +1,35 @@
 package no.falcon.wallasapi.util
 
+import mu.KotlinLogging
 import no.falcon.wallasapi.domain.StatusContent
+import java.lang.Exception
 
 class WallasUtil {
     companion object {
+        private val logger = KotlinLogging.logger {}
+
         private fun parseStatusResponseLine(line: String) = line.split(":")[1].trim()
 
         fun parseStatusResponseString(rawStatusString: String): StatusContent {
-            val parsedString = rawStatusString.replace('\r', '|')
-            val lines = parsedString.split('|')
+            return try {
+                val parsedString = rawStatusString.replace('\r', '|')
+                val lines = parsedString.split('|')
 
-            val setTemp = parseStatusResponseLine(lines[2]).replace("C", "")
-            val readTemp = parseStatusResponseLine(lines[3]).replace("C", "")
-            val volt = parseStatusResponseLine(lines[4]).replace("V", "")
+                val setTemp = parseStatusResponseLine(lines[2]).replace("C", "")
+                val readTemp = parseStatusResponseLine(lines[3]).replace("C", "")
+                val volt = parseStatusResponseLine(lines[4]).replace("V", "")
 
-            return StatusContent(
-                rawStatusString,
-                parseStatusResponseLine(lines[0]),
-                setTemp.toDouble(),
-                readTemp.toDouble(),
-                volt.toDouble()
-            )
+                StatusContent(
+                    rawStatusString,
+                    parseStatusResponseLine(lines[0]),
+                    setTemp.toDouble(),
+                    readTemp.toDouble(),
+                    volt.toDouble()
+                )
+            } catch (ex: Exception) {
+                logger.warn { "Failed to parse status response string [value=$rawStatusString]" }
+                StatusContent(rawStatusString, null, null, null, null)
+            }
         }
     }
 }
